6bdbeb479a15f195e187d94ad1713d72b58d6960
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-10-25 01:39:58 +0900

    Add class convertOpecode

--- index.ts
+++ index.ts
@@ -48,63 +48,90 @@ interface Line {
 
 const fs = require('fs');
 
+class convertOpecode{
+    static string2opecode(opecodeString:string):Opecode{
+        switch(opecodeString){
+            case "LIT":
+                return Opecode.LIT
+            case "OPR":
+                return Opecode.OPR
+            case "LOD":
+                return Opecode.LOD
+            case "STO":
+                return Opecode.STO
+            case "CAL":
+                return Opecode.CAL
+            case "INT":
+                return Opecode.INT
+            case "JMP":
+                return Opecode.JMP
+            case "JPC":
+                return Opecode.JPC
+            case "CSP":
+                return Opecode.CSP
+            case "LAB":
+                return Opecode.LAB
+            case "BAD":
+                return Opecode.BAD
+            case "RET":
+                return Opecode.RET
+            default:
+                console.log("Error, invalid opecode.")
+                process.exit(1)
+        }
+    }
+    static opecode2string(opecode:Opecode):string{
+        switch(opecode){
+            case Opecode.LIT:
+                return "LIT"
+            case Opecode.OPR:
+                return "OPR"
+            case Opecode.LOD:
+                return "LOD"
+            case Opecode.STO:
+                return "STO"
+            case Opecode.CAL:
+                return "CAL"
+            case Opecode.INT:
+                return "INT"
+            case Opecode.JMP:
+                return "JMP"
+            case Opecode.JPC:
+                return "JPC"
+            case Opecode.CSP:
+                return "CSP"
+            case Opecode.LAB:
+                return "LAB"
+            case Opecode.BAD:
+                return "BAD"
+            case Opecode.RET:
+                return "RET"
+            default:
+                console.log("Error, invalid opecode.")
+                process.exit(1)
+        }
+    }
+}
+
 class PL0 {
     str: string
     code: Line[] = []
     constructor(str:string){
-        this.str = str
-    }
-    constructCode():void{
-        const strs:string[] = this.str.split('\n')
-        const returnOpecode = (opecodeString:string):Opecode=>{
-            switch(opecodeString){
-                case "LIT":
-                    return Opecode.LIT
-                case "OPR":
-                    return Opecode.OPR
-                case "LOD":
-                    return Opecode.LOD
-                case "STO":
-                    return Opecode.STO
-                case "CAL":
-                    return Opecode.CAL
-                case "INT":
-                    return Opecode.INT
-                case "JMP":
-                    return Opecode.JMP
-                case "JPC":
-                    return Opecode.JPC
-                case "CSP":
-                    return Opecode.CSP
-                case "LAB":
-                    return Opecode.LAB
-                case "BAD":
-                    return Opecode.BAD
-                case "RET":
-                    return Opecode.RET
-                default:
-                    console.log("Error, read file is incorrect syntax.")
-                    process.exit(1)
-            }
-        }
-        
+        const strs:string[] = str.split('\n')
         strs.forEach((str)=>{
             const splitedStr:string[] = str .replace('(', ',')
                                             .replace(')', ',')
                                             .replace(/ /g, '')
                                             .split(",")
-            const opecode:Opecode = returnOpecode(splitedStr[1])
+            const opecode:Opecode = convertOpecode.string2opecode(splitedStr[1])
             const l:number = parseInt(splitedStr[2])
             const a:number = parseInt(splitedStr[3])
             this.code.push({opecode,l,a})
         })
     }
     print(){
-        console.log(this.str)
-    }
-    printCode(){
         this.code.forEach((line)=>{
-            console.log(`(${line.opecode},${line.l},${line.a})`)
+            console.log(`( ${convertOpecode.opecode2string(line.opecode)},${('    '+line.l).slice(-4)}, ${('    '+line.a).slice(-4)} )`)
         })
     }
 }
@@ -117,9 +144,7 @@ const main = ()=>{
     const str:string = fs.readFileSync(process.argv[2], {encoding: "utf-8"});
 
     const pl0 = new PL0(str)
-    pl0.constructCode()
-//    pl0.print()
-    pl0.printCode()
+    pl0.print()
 
     return 0
 }
